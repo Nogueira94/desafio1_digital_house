142c40cc2b7f833e253b0efff8175ae80f111674
Guilherme Nogueira <guilherme.nogueira11@fatec.sp.gov.br>
2020-09-25 22:34:09 -0300

    fix

--- src/desafio1/Professor.kt
+++ src/desafio1/Professor.kt
@@ -1,4 +1,4 @@
 package desafio1
 
-abstract class Professor(val codProfessor: Int, val nome: String, val sobrenome: String, val tempoCasa: Int) {
+abstract class Professor(val codProfessor: Int, val nome: String, val sobrenome: String, val tempoCasa: Int? = 0) {
 }
