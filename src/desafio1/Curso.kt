cdbfeb3fe0be4c1df2a00092f970b172304c0151
Guilherme Nogueira <guilherme.nogueira11@fatec.sp.gov.br>
2020-09-25 22:26:45 -0300

    fix

--- src/desafio1/Curso.kt
+++ src/desafio1/Curso.kt
@@ -1,7 +1,8 @@
 package desafio1
 
-class Curso(val codCurso: Int, val nome: String, val professorTitular: ProfessorTitular, val professorAdjunto: ProfessorAdjunto, val qntAlunos: Int, val alunosMat: MutableList<Aluno>) {
-
+class Curso(val codCurso: Int, val nome: String, val professorTitular: ProfessorTitular, val professorAdjunto: ProfessorAdjunto, val qntAlunos: Int) {
+    //lateinit var alunosMat: MutableList<Aluno>;
+    var alunosMat = mutableListOf<Aluno>();
     fun adicionarUmAluno(aluno: Aluno): Boolean {
         if ((alunosMat.count() - 1) < qntAlunos) {
             alunosMat.add(aluno)
