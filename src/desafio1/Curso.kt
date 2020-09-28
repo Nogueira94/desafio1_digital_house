6134b738da184973c01f9fc5fce5e05deee9003a
Guilherme Nogueira <guilherme.nogueira11@fatec.sp.gov.br>
2020-09-27 22:02:36 -0300

    ParteI

--- src/desafio1/Curso.kt
+++ src/desafio1/Curso.kt
@@ -1,8 +1,15 @@
 package desafio1
 
-class Curso(val codCurso: Int, val nome: String, val professorTitular: ProfessorTitular, val professorAdjunto: ProfessorAdjunto, val qntAlunos: Int) {
+class Curso(val codCurso: Int, val nome: String, val professorTitular: ProfessorTitular? = null, val professorAdjunto: ProfessorAdjunto? = null, val qntAlunos: Int) {
     //lateinit var alunosMat: MutableList<Aluno>;
     var alunosMat = mutableListOf<Aluno>();
+
+    override fun equals(other: Any?): Boolean {
+        return if (other is Curso){
+            other.codCurso == this.codCurso
+        } else false
+    }
+
     fun adicionarUmAluno(aluno: Aluno): Boolean {
         if ((alunosMat.count() - 1) < qntAlunos) {
             alunosMat.add(aluno)
--- src/desafio1/DigitalHouseManager.kt
+++ src/desafio1/DigitalHouseManager.kt
@@ -1,4 +1,39 @@
 package desafio1
 
-class DigitalHouseManager (val alunos : MutableList<Aluno>, val professores : MutableList<Professor>, val cursos : MutableList<Curso>, val matriculas : MutableList<Matricula> ) {
+class DigitalHouseManager(var alunos: MutableList<Aluno>, var professores: MutableList<Professor>, var cursos: MutableList<Curso>, var matriculas: MutableList<Matricula>) {
+
+    fun registrarCurso(nome: String, codigoCurso: Int, quantidadeMaximaDeAlunos: Int) {
+        val curso = Curso(codigoCurso,nome,qntAlunos = quantidadeMaximaDeAlunos)
+        if (cursos.contains(curso)) {
+            println("Codigo de curso ja existente")
+        } else cursos.add(curso)
+    }
+
+    fun excluirCurso(codigoCurso: Int) {
+
+    }
+
+    fun registrarProfessorAdjunto(nome: String, sobrenome: String, codigoProfessor: Int, quantidadeDeHoras: Int) {
+
+    }
+
+    fun registrarProfessorTitular(nome: String, sobrenome: String, codigoProfessor: Int, especialidade: String) {
+
+    }
+
+    fun excluirProfessor(codigoProfessor: Int) {
+
+    }
+
+    fun matricularAluno(nome: String, sobrenome: String, codigoAluno: Int) {
+
+    }
+
+    fun matricularAluno(codigoAluno: Int, codigoCurso: Int) {
+
+    }
+
+    fun alocarProfessores(codigoCurso: Int, codigoProfessorTitular: Int, codigoProfessorAdjunto: Int) {
+
+    }
 }
